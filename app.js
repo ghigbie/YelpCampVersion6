8168b61085206a12fbc2e89a82fb7284dfdea4c3
ghigbie <gehigbie@gmail.com>
2017-04-05 01:33:25 +0000

    Add seeds.js file

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express    = require("express"),
-      app        = express(),
-      bodyParser = require("body-parser"),
-      mongoose   = require("mongoose"),
-      passport   = require("passport");
+const express       = require("express"),
+      app           = express(),
+      bodyParser    = require("body-parser"),
+      mongoose      = require("mongoose"),
+      passport      = require("passport"),
+      LocalStrategy = require("passport-local");
       
-     // LocalStrategy =
 const Campground  = require("./modles/campground"),
       Comment     = require("./modles/comment"),
+      User        = require("./models/user"),
       seedDB      = require("./seeds");
         
