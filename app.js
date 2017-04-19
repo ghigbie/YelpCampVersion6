28acf9a10e14c7fa2c14d87ca404daa969b43198
ghigbie <gehigbie@gmail.com>
2017-04-19 23:20:17 +0000

    Add comments routes

--- app.js
+++ app.js
@@ -81,6 +81,42 @@ app.get("/campgrounds/:id", (req, res) => {
    }); 
 });
 
+// ======================
+// COMMENTS ROUTE
+// ======================
+app.get("/campgrounds/:id/comments/new", (req, res) =>{
+   //find campground by id
+   Campground.findById(req.params.id, (err, campground) => {
+       if(err){
+           console.log(err);
+       }else{
+          res.render("comments/new", {campground: campground});
+       }
+   });
+});
+
+app.post("/campgrounds/:id/comments", (req, res) => {
+    //lookup campground using ID
+    Campground.findById(req.params.id, (err, campground) => {
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        }else{
+            console.log(req.body.commet);
+            Comment.create(req.body.comment, (err, comment) => { //create a new comment
+                if(err){
+                    console.log(err);
+                }else{
+                    campground.comments.push(comment); //connect new commet to campground
+                    campground.save();
+                    res.redirect("/campgroounds/" + campground._id); //redirect campground show page
+                }
+            });
+        }
+    });
+});
+
+
 //=============
 //AUTH ROUTES
 //=============
